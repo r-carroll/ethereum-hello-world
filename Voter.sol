3940de877ffb84ca11611d1566094d56869311ee
Ryan Carroll <r.carroll@nationwide.com>
2021-05-11 13:25:22 -0400

    adding empty voter structure

---
+++ Voter.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+contract Voter {
+
+    constructor(string[] memory _options) public {
+
+    }
+
+    function vote(uint option) public {
+
+    }
+
+    function getOptions() public view returns (string[] memory) {
+
+    }
+
+    function getVotes() public view returns (uint[] memory) {
+    }
+}
